67029cdbacf83b7e3625b986e70af718893e26eb
NowApoca <manueldiez123@gmail.com>
2020-05-01 17:55:53 -0300

    server

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const router = require(__dirname + "./routes/routes")
+const bodyParser = require("body-parser")
+const db = require("./src/database/database")
+const fs = require("fs")
+
+async function initialize(setupFile){
+    const settings = JSON.parse(fs.readFileSync(setupFile))
+    await db.initialize(settings.db, settings.dbConfig)
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+    app.use("", router)
+    app.listen(settings.port)
+}
+
+if (!module.parent) {
+    initialize(process.argv[2])
+} 
+
+module.exports = {
+    initialize
+}
