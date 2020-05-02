02f451a3e00f07be9ae25ae282f18a1f00e45152
Alejo <alejo@Alejo.local>
2020-05-02 14:43:34 -0300

    post and get product

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -37,27 +37,42 @@ const constants = require ("../config").constants;
 
 async function create(req,res){
     const client = getClient();
-    const {token , filters } = res.locals;
-    if((await client.query('SELECT id from product WHERE id = $1;',[id])).rows.length < 0){
-        throw new Error({});
-    }
-    const productsInDb = await client.query('SELECT id from product WHERE id=$1;',[id]);
-    const products =[];
-    productsInDb.rows.map((product,index)=>{
-        products.push({
-            product
-        });
-    });
-    res.status(200).json(products);
+    const{
+      id,
+      name,
+      type,
+      price,
+      additionalOptions,
+      description,
+      image,
+    } = res.locals;
+    //check product id valid
+    if((await client.query('SELECT id from product WHERE product = $1;',[id])).rows.length>0){
+      throw new Error({})
+    };
+    //Create product
+    await client.query('INSERTO INTO product (id,name,type,price,additionalOptions,description,image) VALUES $1;',[
+      id,
+      name,
+      type,
+      price,
+      additionalOptions,
+      description,
+      image
+    ]);
+    res.status(200);
 }; 
+
 
 async function getList(req,res){
   const client = getClient();
   const {token , filters } = res.locals;
-  if((await client.query('SELECT id from product WHERE id = $1;',[id])).rows.length < 0){
-      throw new Error({});
-  }
-  const productsInDb = await client.query('SELECT id from product WHERE id=$1;',[id]);
+  const userInDB = await client.query('SELECT email FROM user WHERE token = $1;',[token]);
+  if(userInDB.row.length<0){
+    throw new Error({})
+  };
+  //to review
+  const productsInDb = await client.query('SELECT * from product WHERE type=$1;',[filters.type]);
   const products =[];
   productsInDb.rows.map((product,index)=>{
       products.push({
@@ -66,3 +81,4 @@ async function getList(req,res){
   });
   res.status(200).json(products);
 }; 
+
